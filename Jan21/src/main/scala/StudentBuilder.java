d64f14c65f57f1d168b72b6ded4f386adc33ba5d
shashwath <shashwath44@gmail.com>
2021-01-27 20:51:00 -0600

    Test commit 2: included the last name of the student

--- Jan21/src/main/scala/StudentBuilder.java
+++ Jan21/src/main/scala/StudentBuilder.java
@@ -2,7 +2,8 @@ public class StudentBuilder {
     public class Student {
         private float gpa = 0;
         private int uid = 0;
-        private String name = "";
+        private String fName = "";
+        private String lName = "";
     }
     
     
